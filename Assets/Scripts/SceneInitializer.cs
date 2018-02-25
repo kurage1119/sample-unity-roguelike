23009aef537ec3d003fae8594ec18d44196d2620
Keisuke Kuroda <kurage1119@gmail.com>
2018-02-26 05:00:41 +0900

    マップを3Dオブジェクトで構築するロジックの追加 * Unityちゃんアセットが必要です。公式からダウンロード、インポートしてください

--- Assets/Scripts/SceneInitializer.cs
+++ Assets/Scripts/SceneInitializer.cs
@@ -1,5 +1,5 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class SceneInitializer : MonoBehaviour {
 
@@ -8,10 +8,24 @@ public class SceneInitializer : MonoBehaviour {
 
 	public const int MAX_ROOM_NUMBER = 6;
 
+	public GameObject _player;
+
+	private GameObject floorPrefab;
+	private GameObject wallPrefab;
+
+	private int[,] map;
+
 	// Use this for initialization
 	void Start () {
 
-		int[,] map = new MapGenerator().GenerateMap(MAP_SIZE_X, MAP_SIZE_Y, MAX_ROOM_NUMBER);
+		GenerateMap();
+
+		SponePlayer();
+
+	}
+
+	private void GenerateMap() {
+		map = new MapGenerator().GenerateMap(MAP_SIZE_X, MAP_SIZE_Y, MAX_ROOM_NUMBER);
 
 		string log = "";
 		for (int y = 0; y < MAP_SIZE_Y; y++) {
@@ -22,6 +36,41 @@ public class SceneInitializer : MonoBehaviour {
 		}
 		Debug.Log(log);
 
+		floorPrefab = Resources.Load("Prefabs/Floor") as GameObject;
+		wallPrefab = Resources.Load("Prefabs/Wall") as GameObject;
+
+		var floorList = new List<Vector3>();
+		var wallList = new List<Vector3>();
+
+		for (int y = 0; y < MAP_SIZE_Y; y++) {
+			for (int x = 0; x < MAP_SIZE_X; x++) {
+				if (map[x, y] == 1) {
+					// Instantiate(floorPrefab, new Vector3(x, 0, y), new Quaternion());
+					floorList.Add(new Vector3(x, 0, y));
+				} else {
+					// Instantiate(wallPrefab, new Vector3(x, 0, y), new Quaternion());
+					wallList.Add(new Vector3(x, 0, y));
+				}
+			}
+		}
+
+		foreach (var vector in floorList) {
+			Instantiate(floorPrefab, vector, new Quaternion());
+		}
+		foreach (var vector in wallList) {
+			Instantiate(wallPrefab, vector, new Quaternion());
+		}
+	}
+
+	private void SponePlayer() {
+		Position position;
+		do {
+			var x = RogueUtils.GetRandomInt(0, 	MAP_SIZE_X - 1);
+			var y = RogueUtils.GetRandomInt(0, 	MAP_SIZE_Y - 1);
+			position = new Position(x, y);
+		} while (map[position.X, position.Y] != 1);
+
+		_player.transform.position = new Vector3(position.X, 0, position.Y);
 	}
 
 }
